6e3b23cc6ca1e7de2b8fdd9a48012f497238ab2c
a2d7cc <a2d7cc@gmail.com>
2022-11-12 15:41:26 +0100

    Prettier sort imports

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import Header from '@/components/Header'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+
+import Header from '@/components/Header'
 
 export default function App() {
 	return (
