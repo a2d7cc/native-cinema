f67cdb39dc5708c12beae6e85f6a53458d2ffa84
a2d7cc <a2d7cc@gmail.com>
2022-11-15 01:13:03 +0100

    Auth Fields

--- app/components/screens/auth/Auth.tsx
+++ app/components/screens/auth/Auth.tsx
@@ -1,40 +1,53 @@
-import { Button, Loader } from '@/components/ui'
-import { IAuthFormData } from '@/shared/types/auth.interface'
 import { FC, PropsWithChildren, useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { Pressable, Text, View } from 'react-native'
 
+import { Button, Loader } from '@/components/ui'
+import DismissKeyboard from '@/components/ui/form-elements/field/DismissKeyboard'
+
+import { IAuthFormData } from '@/shared/types/auth.interface'
+
+import AuthFields from './AuthFields'
+
 const Auth: FC<PropsWithChildren> = ({ children }) => {
 	const [isReg, setIsReg] = useState(false)
-	const { handleSubmit, reset, control } = useForm({
+	const { handleSubmit, reset, control } = useForm<IAuthFormData>({
 		mode: 'onChange'
 	})
 
-	const onSubmit:SubmitHandler<IAuthFormData> = ({email, password}) => {} 
+	const onSubmit: SubmitHandler<IAuthFormData> = ({ email, password }) => {
+		console.log(email, password)
+	}
 
 	const isLoading = false
 
 	return (
-		<View  className='mx-2 items-center justify-center h-full'>
-			<View className='w-9/12'>
-				<Text className='text-center text-white text-4xl font-bold mb-2.5'>
-					{isReg? 'Register' : 'Login'}
-				</Text>
-				{isLoading ? <Loader /> : <>
-				{/* Fields */}
-				<Button onPress={handleSubmit(onSubmit)} icon={'film'}>
-					Go to watch	
-				</Button>		
+		<DismissKeyboard>
+			<View className='mx-2 items-center justify-center h-full'>
+				<View className='w-9/12'>
+					<Text className='text-center text-white text-4xl font-bold mb-2.5'>
+						{isReg ? 'Register' : 'Login'}
+					</Text>
+					{isLoading ? (
+						<Loader />
+					) : (
+						<>
+							<AuthFields control={control} isPassRequired />
 
+							<Button onPress={handleSubmit(onSubmit)} icon={'film'}>
+								Go to watch
+							</Button>
 
-			<Pressable onPress={() => setIsReg(!isReg)}>
-				<Text className='text-white opacity-30 text-right text-base mt-3'>
-					{isReg? 'Login' : 'Register'}
-				</Text>
-			</Pressable>
-				</>}
+							<Pressable onPress={() => setIsReg(!isReg)}>
+								<Text className='text-white opacity-30 text-right text-base mt-3'>
+									{isReg ? 'Login' : 'Register'}
+								</Text>
+							</Pressable>
+						</>
+					)}
+				</View>
 			</View>
-		</View>
+		</DismissKeyboard>
 	)
 }
 
---
+++ app/components/screens/auth/AuthFields.tsx
@@ -0,0 +1,52 @@
+import { FC } from 'react'
+import { Control } from 'react-hook-form'
+import { Text, View } from 'react-native'
+
+import Field from '@/components/ui/form-elements/field/Field'
+
+import { validEmail } from '@/shared/regex'
+import { IAuthFormData } from '@/shared/types/auth.interface'
+
+interface IAuthFields {
+	control: Control<IAuthFormData>
+	isPassRequired?: boolean
+}
+
+const AuthFields: FC<IAuthFields> = ({ control, isPassRequired }) => {
+	return (
+		<>
+			<Field<IAuthFormData>
+				placeholder='Enter E-Mail'
+				control={control}
+				name='email'
+				rules={{
+					required: 'Email',
+					pattern: {
+						value: validEmail,
+						message: 'Please enter a valid email address'
+					}
+				}}
+				keyboardType='email-address'
+			/>
+
+			<Field<IAuthFormData>
+				placeholder='Enter a password'
+				control={control}
+				name='password'
+				rules={
+					isPassRequired
+						? {
+								required: 'Password is required!',
+								minLength: {
+									value: 6,
+									message: 'Password should be minimum 6 characters long'
+								}
+						  }
+						: {}
+				}
+			/>
+		</>
+	)
+}
+
+export default AuthFields
