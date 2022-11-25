dcb53c8d3a7c3014a353649caa076e93bbbcb38f
a2d7cc <a2d7cc@gmail.com>
2022-11-25 22:28:10 +0100

    Animated Carousel

--- app/components/screens/home/carousel/carousel-item/CarouselItem.tsx
+++ app/components/screens/home/carousel/carousel-item/CarouselItem.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { Image, Pressable, Text, View } from 'react-native'
+import { Animated, Image, Pressable, Text, View } from 'react-native'
 
 import { GenreList, Rating } from '@/components/ui'
 
@@ -9,40 +9,73 @@ import { IMovie } from '@/shared/types/movie.interface'
 
 import { useGetMediaSource } from '@/utils/useGetMediaSource'
 
-const CarouselItem: FC<{ movie: IMovie }> = ({ movie }) => {
+import { ITEM_SIZE, SPACING } from '../carousel.constants'
+
+import { useItemAnimation } from './useItemAnimation'
+
+interface ICarouselItem {
+	index: number
+	scrollX: Animated.Value
+	movie: IMovie
+}
+
+const CarouselItem: FC<ICarouselItem> = ({ movie, index, scrollX }) => {
 	const { navigate } = useTypedNavigation()
 
+	const { rotate, opacity, scale, opacityElements } = useItemAnimation(
+		index,
+		scrollX
+	)
+
 	return (
-		<View className='w-48'>
-			<Pressable onPress={() => navigate('Movie', { slug: movie.slug })}>
-				<View className='absolute z-1 right-2 top-2'>
-					{/* Favorite Button */}
-				</View>
+		<View
+			style={{
+				width: ITEM_SIZE
+			}}
+		>
+			<Animated.View
+				style={{
+					padding: SPACING,
+					transform: [{ rotate }, { scale }],
+					opacity
+				}}
+				className={'items-center'}
+			>
+				<Pressable
+					className='w-full relative'
+					onPress={() => navigate('Movie', { slug: movie.slug })}
+				>
+					<View className='absolute z-1 right-2 top-2'>
+						{/* Favorite Button */}
+					</View>
 
-				<Image
-					style={{
-						height: 200,
-						resizeMode: 'cover',
-						borderWidth: 1,
-						borderColor: 'white'
-					}}
-					className='w-full rounded-xl mb-2.5'
-					source={useGetMediaSource(movie.poster)}
-				/>
-			</Pressable>
-			<View className='items-center'>
-				<Pressable onPress={() => navigate('Movie', { slug: movie.slug })}>
-					<Rating rating={movie.rating} />
-					<Text
-						className='text-white text-3xl font-semibold opacity-95 mb-2.5'
-						numberOfLines={1}
-					>
-						{movie.title}
-					</Text>
+					<Image
+						style={{
+							height: ITEM_SIZE * 1.3,
+							resizeMode: 'cover',
+							borderWidth: 1,
+							borderColor: 'white'
+						}}
+						className='w-full rounded-xl mb-2.5'
+						source={useGetMediaSource(movie.poster)}
+					/>
 				</Pressable>
+				<Animated.View className='items-center' style={{
+					opacity: opacityElements
+				}}>
+					<Pressable onPress={() => navigate('Movie', { slug: movie.slug })}>
+						<Rating rating={movie.rating} />
+						<Text
+							className='text-white text-3xl font-semibold opacity-95 mb-2.5'
+							numberOfLines={1}
+						>
+							{movie.title}
+						</Text>
+					</Pressable>
 
-				<GenreList genres={movie.genres} />
-			</View>
+					<GenreList genres={movie.genres} />
+				</Animated.View>
+			</Animated.View>
 		</View>
 	)
 }
