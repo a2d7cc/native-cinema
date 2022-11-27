6935dce9c525c5d1a7083eaa83b45717887a9dec
a2d7cc <a2d7cc@gmail.com>
2022-11-27 21:01:38 +0100

    Search page

--- app/components/screens/search/Search.tsx
+++ app/components/screens/search/Search.tsx
@@ -1,12 +1,35 @@
-import {FC} from "react"
-import { Text, View } from 'react-native'
+import { FC } from 'react'
+import { View } from 'react-native'
+
+import { Field, Heading, Layout, Loader, MovieCatalog } from '@/components/ui'
+
+import { ISearchFormData } from './search.interface'
+import { useSearch } from './useSearch'
 
 const Search: FC = () => {
-	return (
-		<View>
-		 <Text>Search</Text>
-		</View>
-	)
-};
+	const { searchTerm, movies, isLoading, control } = useSearch()
+	console.log(isLoading)
+	console.log(searchTerm)
+	console.log(movies)
 
-export default Search;
+	return (
+		<Layout isHasPadding>
+			<Heading title='Search' />
+			<View className='mt-3'>
+				<Field<ISearchFormData>
+					placeholder='Type something...'
+					control={control}
+					name='searchTerm'
+					keyboardType='web-search'
+				/>
+			</View>
+			{!!searchTerm ? (
+				<View className='mt-3'>
+					{isLoading ? <Loader /> : <MovieCatalog title='' movies={movies} />}
+				</View>
+			) : null}
+		</Layout>
+	)
+}
+
+export default Search
